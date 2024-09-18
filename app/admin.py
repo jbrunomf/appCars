535bb394f78e76e992f021c6483e6756f320ee7e
João Bruno M. Fernandes <jbrunomf@outlook.com>
2024-09-18 19:59:18 -0400

    Change language to pt-BR and add Car model with admin features
    Updated `LANGUAGE_CODE` to `pt-BR` to support Portuguese-Brazil. Added a `Car` model and registered it in the admin interface with display, search, filter, and sorting capabilities.

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Car
+
 
 # Register your models here.
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ['id', 'model', 'brand', 'factory_year', 'model_year', 'value']
+    search_fields = ['model', 'brand', 'factory_year', 'model_year', 'value']
+    list_filter = ['brand']
+    list_per_page = 10
+    list_max_show_all = 20
+    sortable_by = ['model', 'brand', 'factory_year', 'model_year', 'value']
--- app/models.py
+++ app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Car(models.Model):
+    id = models.AutoField(primary_key=True)
+    model = models.CharField(max_length=120)
+    brand = models.CharField(max_length=120)
+    factory_year = models.IntegerField()
+    model_year = models.IntegerField()
+    value = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.model
+
+
+
