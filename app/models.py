dc5e01a64dd4d17e4014653895502ae82e519d5d
João Bruno M. Fernandes <jbrunomf@outlook.com>
2024-09-18 20:09:22 -0400

    Add Brand model and update Car brand relation
    Introduced a new Brand model with ID and name fields. Updated the Car model to use a ForeignKey relationship for the brand with the on_delete set to PROTECT to ensure referential integrity.

--- app/models.py
+++ app/models.py
@@ -1,15 +1,21 @@
 from django.db import models
 
+
+class Brand(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=120)
+
+    def __str__(self):
+        return self.name
+
+
 class Car(models.Model):
     id = models.AutoField(primary_key=True)
     model = models.CharField(max_length=120)
-    brand = models.CharField(max_length=120)
     factory_year = models.IntegerField()
     model_year = models.IntegerField()
     value = models.DecimalField(max_digits=10, decimal_places=2)
+    brand = models.ForeignKey('Brand', on_delete=models.PROTECT, related_name='cars')
 
     def __str__(self):
         return self.model
-
-
-
