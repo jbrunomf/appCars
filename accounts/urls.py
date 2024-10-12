adfb77b5479f3845e7fc24962acc2c45a7b1fdcd
João Bruno M. Fernandes <jbrunomf@outlook.com>
2024-10-12 11:09:01 -0400

    Add user registration functionality to accounts
    Implemented user registration view and template. Modified the URL configurations to include the new registration route and integrated Bootstrap for styling in the base template.

--- accounts/urls.py
+++ accounts/urls.py
@@ -14,14 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from webbrowser import register
+
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from .views import register_view
 
 from appCars import settings
 
 urlpatterns = [
-                  path('admin/', admin.site.urls),
-                  path('cars/', include('app.urls')),
-                  path('accounts/', include('accounts.urls')),
+                  path('register/', register_view, name='accounts_register'),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
+def register_view(request):
+    if request.method == 'POST':
+        user_form = UserCreationForm(request.POST)
+
+        if user_form.is_valid():
+            user_form.save()
+
+            return redirect('car_list')
+        else:
+            messages.add_message(request, messages.INFO, 'Erro ao cadastrar usuário')
+            return redirect('accounts_register')
+    else:
+        user_form = UserCreationForm()
+        return render(request, 'accounts/register.html', {'user_form': user_form})
